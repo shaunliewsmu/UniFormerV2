d3e7965cc114cc4d4e60a780846a22b2a75945e7
Shaun Liew Xin Hong <shaunliew@smu.edu.sg>
2025-04-10 14:44:20 +0800

    improve the custom script

--- exp/custom_referral/custom_b16_f8x224/test.sh
+++ exp/custom_referral/custom_b16_f8x224/test.sh
@@ -4,6 +4,11 @@ work_path=$(dirname $0)
 NUM_SHARDS=1
 NUM_GPUS=1  # Set to 0 to avoid distributed training issues
 BATCH_SIZE=8
+SAMPLING_METHOD="uniform"
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+OUTPUT_DIR="${work_path}/output/test_${TIMESTAMP}_${SAMPLING_METHOD}"
+
+export CUDA_VISIBLE_DEVICES=1
 
 PYTHONPATH=$PYTHONPATH:./slowfast \
 python3 tools/custom_test_net.py \
@@ -11,14 +16,14 @@ python3 tools/custom_test_net.py \
   --cfg $work_path/config.yaml \
   DATA.PATH_TO_DATA_DIR ./data_list/custom_referral \
   DATA.PATH_PREFIX data/duhs-gss-split-5:v0/organized_dataset \
-  DATA.SAMPLING_METHOD "random" \
+  DATA.SAMPLING_METHOD ${SAMPLING_METHOD} \
   DATA.PATH_LABEL_SEPARATOR " " \
   TRAIN.ENABLE False \
   TEST.ENABLE True \
   TEST.NUM_ENSEMBLE_VIEWS 4 \
   TEST.NUM_SPATIAL_CROPS 3 \
-  TEST.CHECKPOINT_FILE_PATH $work_path/output/best.pyth \
+  TEST.CHECKPOINT_FILE_PATH $work_path/output/run_20250307_155856_uniform/best.pyth \
   TEST.ADD_SOFTMAX True \
   NUM_GPUS $NUM_GPUS \
   NUM_SHARDS $NUM_SHARDS \
-  OUTPUT_DIR $work_path/output
+  OUTPUT_DIR $OUTPUT_DIR
